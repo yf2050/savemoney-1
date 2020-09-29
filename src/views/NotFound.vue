401d3fb5d9f8971054e9a49ce238cdacf412a51c
vue <vue000@hotmail.com>
2020-09-29 23:41:13 +0800

    添加404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>404页面 请跳转新页面</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
