5a589d606c4b532243dcd3fca46db32b4623e5ae
vue <vue000@hotmail.com>
2020-10-07 22:49:25 +0800

    创建 <Icon /> 封装组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Nav'
-}
+// import x from '@/assets/icons/labels.svg'
+// import y from '@/assets/icons/money.svg'
+// console.log(x)
+// console.log(y)
+
+export default {name: 'Nav'}
 </script>
 
 <style lang="scss" scoped>
-.nav{border:1px solid red}
+.nav {border: 1px solid red}
 </style>
