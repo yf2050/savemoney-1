62e96cf9f04028ba2a9f68349e7ea793b94fa775
vue <vue000@hotmail.com>
2020-10-21 19:12:10 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -5,39 +5,14 @@
 </template>
 
 <style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 
 body {
   line-height: 1.5;
-}
-
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:$font-hei;
+  color: #333;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  //text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,6 +23,7 @@ export default {name: 'Nav'}
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -30,7 +31,7 @@ nav {
   font-size: 12px;
 
   > .item.selected {
-    color: red;
+    color: $color-highlight;
   }
 
   > .item {
