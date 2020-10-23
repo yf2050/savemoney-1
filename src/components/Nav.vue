a6dc3474733014cfe8773cebce97f33ab94e35a2
vue <vue000@hotmail.com>
2020-10-23 16:31:51 +0800

    添加css样式---数字格、阴影

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -25,8 +25,8 @@ export default {name: 'Nav'}
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 nav {
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   justify-content: space-around;
   font-size: 12px;
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -37,8 +37,8 @@
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>OK</button>
-        <button>0</button>
+        <button class="ok">OK</button>
+        <button class="zero">0</button>
         <button>.</button>
       </div>
     </div>
@@ -58,7 +58,7 @@ export default {
   font-size: 14px;
   padding: 16px;
 
-  > .current {
+  .current {
     display: flex;
 
     > li {
@@ -109,6 +109,7 @@ export default {
   display: flex;
   text-align: center;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -116,15 +117,67 @@ export default {
     align-items: center;
     justify-content: center;
     position: relative;
-    &.selected::after{
+
+    &.selected::after {
       content: '';
       position: absolute;
-      bottom:0;
+      bottom: 0;
       left: 0;
       width: 100%;
       height: 4px;
       background: #333;
     }
+  }
+}
+
+.numberPad {
+  .output {
+    @extend %clearFix;
+    @extend %innerShadow;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+    text-align: right;
+  }
+
+  .buttons {
+    @extend %clearFix;
+     > button{
+       width: 25%;
+       height: 64px;
+       float: left;
+       background: transparent;
+       border:none;
+       &.ok{
+         height: 64*2px;
+         float:right;
+       }
+       &.zero{
+         width: 25*2%;
+       }
+       $bg:#F2F2F2;
+       &:nth-child(1){
+         background: $bg;
+       }
+       &:nth-child(2),&:nth-child(5){
+         background: darken($bg,4%);
+       }
+       &:nth-child(3),&:nth-child(6),&:nth-child(9){
+         background: darken($bg,4*2%);
+       }
+       &:nth-child(4),&:nth-child(7),&:nth-child(10){
+         background: darken($bg,4*3%);
+       }
+       &:nth-child(8),&:nth-child(11),&:nth-child(13){
+         background: darken($bg,4*4%);
+       }
+       &:nth-child(14){
+         background: darken($bg,4*5%);
+       }
+       &:nth-child(12){
+         background: darken($bg,4*6%);
+       }
+     }
   }
 }
 </style>
