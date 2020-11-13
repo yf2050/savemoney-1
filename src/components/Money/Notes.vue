304b931e12a4f86a8bff928fe8d8cf430cc9fb01
vue <vue000@hotmail.com>
2020-11-13 23:46:46 +0800

    模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注">
+      </label>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+}
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background-color: #f5f5f5;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+
+  > .name {
+    padding: 0 16px;
+  }
+
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+  }
+}
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Tags'
+}
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column-reverse;
+  font-size: 14px;
+  padding: 16px;
+
+  .current {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      background-color: #D9D9D9;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      //border-radius:$h/2;
+      border-radius: $h/2;
+      padding: 0 17px 0 18px;
+      margin-right: 16px;
+      margin-top: 4px;
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      color: #999999;
+      border-bottom: 1px solid;
+      padding: 0 3px;
+    }
+  }
+}
+</style>
