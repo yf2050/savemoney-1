16ec0eb57a2498333d0f4bb6e7582dbd8b123f71
vue <vue000@hotmail.com>
2021-03-15 20:19:18 +0800

    部署网站到github的savemoney仓库

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/morney-3-website/'
+    ? '/savemoney/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
